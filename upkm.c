e5b7395840a8bd3f4e529205c8027c8a2e879973
Dan <dalmemail@amaya.tk>
2016-04-05 21:59:56 +0200

    UPKM: Versión 0.2

--- upkm.c
+++ upkm.c
@@ -16,27 +16,47 @@
  */
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
 /* fcl.h */
 #include "../fcl.h"
 
-/* upkm v0.1 */
+/* upkm v0.2 */
 
 /* Poketty Version 1.3 */
 #define VERSION "1.3"
 
-int check_pkm(char *input)
-{
-	int result = 0;
-	char pkm[8];
-	get_value_string(input, "pkm", pkm);
-	if ((strcmp(pkm, VERSION)) != 0) {
-		result++;
-	}
-	return result;
-}
+char pokemon_data[24][25];
+
+char to_find[24][25] = {"nombre", "pv", "ataque1", "ataque2", "ataque3", "ataque4",
+			"pd1_max", "pd2_max", "pd3_max", "pd4_max", "pd1_min", "pd2_min",
+			"pd3_min", "pd4_min", "ataque1_estado", "ataque2_estado",
+			"ataque3_estado", "ataque4_estado", "pp1", "pp2", "pp3", "pp4",
+			"pociones", "antidotos"};
+
+int value_string_result[24];
 
 void make_pkm(char *input, char *output)
 {
+	creat(output, 0644);
+	int fd;
+	if ((fd = open(output, O_WRONLY)) < 0) {
+		printf("Error: No se puede abrir %s\n", output);
+	}
+	else {
+		char buffer[40];
+		sprintf(buffer, "pkm=%s\n", VERSION);
+		write(fd, buffer, strlen(buffer));
+		for (int i = 0; i < 24; i++) {
+			value_string_result[i] = get_value_string(input, to_find[i], pokemon_data[i]);
+			if (value_string_result[i] != 0) {
+				//cosas
+			}
+			sprintf(buffer, "%s=%s\n", to_find[i], pokemon_data[i]);
+			write(fd, buffer, strlen(buffer));
+		}
+		close(fd);
+	}
 }
 
 int main(int argc, char **argv)
@@ -45,7 +65,8 @@ int main(int argc, char **argv)
 		printf("Uso: %s input.pkm output.pkm\n", argv[0]);
 	}
 	else {
-		if ((check_pkm(argv[1])) == 0) {
+		char pkm[8];
+		if ((get_value_string(argv[1], "pkm", pkm)) == 0 && (strcmp(pkm, VERSION)) == 0) {
 			printf("Tu archivo ya es compatible con Poketty %s y no debe ser convertido\n", VERSION);
 		}
 		else {
