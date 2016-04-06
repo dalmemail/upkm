c406d509da3ef56af9706b07d4ddb431d9cfc8b5
Dan <dalmemail@amaya.tk>
2016-04-06 20:04:03 +0200

    UPKM: Versión 0.3

--- upkm.c
+++ upkm.c
@@ -16,12 +16,14 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <fcntl.h>
 #include <string.h>
 /* fcl.h */
 #include "../fcl.h"
 
-/* upkm v0.2 */
+/* upkm v0.3 */
 
 /* Poketty Version 1.3 */
 #define VERSION "1.3"
@@ -34,10 +36,48 @@ char to_find[24][25] = {"nombre", "pv", "ataque1", "ataque2", "ataque3", "ataque
 			"ataque3_estado", "ataque4_estado", "pp1", "pp2", "pp3", "pp4",
 			"pociones", "antidotos"};
 
+char new_data[24][25] = {"pikachu", "100", "bola trueno", "placaje", "relampago", "onda trueno",
+			"pd1_max", "pd2_max", "pd3_max", "pd4_max", "pd1_min", "pd2_min",
+			"pd3_min", "pd4_min", "ataque1_estado", "ataque2_estado",
+			"ataque3_estado", "ataque4_estado", "pp1", "pp2", "pp3", "pp4",
+			"4", "5"};
+
+char estados_pokemon[3][25] = {"paralizado", "aturdido", "normal"};
+
 int value_string_result[24];
+
+void make_pokemon_status(int start, int lonng)
+{
+	for (int i = start; i < (lonng+start); i++) {
+		int num = rand() % 3;
+		strcpy(new_data[i], estados_pokemon[num]);
+	}
+}
+
+void make_pokemon_pd(int start, int lonng)
+{
+	for (int i = start; i < (lonng+start); i++) {
+		int num = rand() % 50;
+		sprintf(new_data[i], "%d", num);
+		int num1 = rand() % num;
+		sprintf(new_data[i+lonng], "%d", num1);
+	}
+}
+
+void make_pokemon_pp(int start, int lonng)
+{
+	for (int i = start; i < (lonng+start); i++) {
+		int num = rand() % 40;
+		sprintf(new_data[i], "%d", num);
+	}
+}
 
 void make_pkm(char *input, char *output)
 {
+	srand(time(NULL));
+	make_pokemon_pd(6, 4);
+	make_pokemon_pp(18, 4);
+	make_pokemon_status(14, 4);
 	creat(output, 0644);
 	int fd;
 	if ((fd = open(output, O_WRONLY)) < 0) {
@@ -50,7 +90,8 @@ void make_pkm(char *input, char *output)
 		for (int i = 0; i < 24; i++) {
 			value_string_result[i] = get_value_string(input, to_find[i], pokemon_data[i]);
 			if (value_string_result[i] != 0) {
-				//cosas
+				strcpy(pokemon_data[i], new_data[i]);
+				printf("Creando campo '%s': OK\n", to_find[i]);
 			}
 			sprintf(buffer, "%s=%s\n", to_find[i], pokemon_data[i]);
 			write(fd, buffer, strlen(buffer));
@@ -59,19 +100,26 @@ void make_pkm(char *input, char *output)
 	}
 }
 
-int main(int argc, char **argv)
+int upkm(int argc, char **argv)
 {
+	int result = 0;
 	if (argc < 3) {
 		printf("Uso: %s input.pkm output.pkm\n", argv[0]);
 	}
 	else {
-		char pkm[8];
+		char pkm[64];
 		if ((get_value_string(argv[1], "pkm", pkm)) == 0 && (strcmp(pkm, VERSION)) == 0) {
 			printf("Tu archivo ya es compatible con Poketty %s y no debe ser convertido\n", VERSION);
 		}
 		else {
-			make_pkm(argv[1], argv[2]);
+			if ((get_value_string(argv[1], "nombre", pkm)) == 0) {
+				make_pkm(argv[1], argv[2]);
+			}
+			else {
+				printf("El archivo '%s' es incompatible con Poketty 1.0\n", argv[1]);
+				result--;
+			}
 		}
 	}
-	return 0;
+	return result;
 }
